b5a991f4013318d239b1d8e36cae1474d9999341
amitay biton <amitaybiton@gmail.com>
2021-12-17 12:54:40 +0200

    added templates for API with TODOs

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  // TODO: query all users from SQL to JSON and send as response
+  res.send(`all users`);
 });
+
+router.get('/:userId', function(req, res) {
+  // TODO: query all users from SQL to JSON and send as response
+  res.send(`userid = ${req.params.userId}`)
+});
+
 
 module.exports = router;
