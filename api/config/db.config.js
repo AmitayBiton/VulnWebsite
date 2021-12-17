0a8bee4ba73d50e36e7b624b3f880b68db11077b
Tomer Mekler <tomermk@gmail.com>
2021-12-17 17:51:34 +0200

    Update db.config.js

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "BalonGadol1@",
+    USER: "******",
+    PASSWORD: "********",
     DB: "vulnsite",
     dialect: "mysql",
     pool: {
