443ed9c1b237b5540de2f55835f42321e7741651
Tomermk <tomermk@gmail.com>
2021-12-24 02:12:04 +0200

    Not yet working

--- api/app.js
+++ api/app.js
@@ -35,14 +35,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors ({
-  origin: '*'
+  origin: ["https://localhost:3000"],
+  credentials: true
 }));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(sessions({
+    key: "userId",
     secret: "thisisatestkeyitsnotthatimportantrightnow123",
     store: sessionStore,
     saveUninitialized:true,
