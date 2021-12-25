b748a7f27bc9fd5575c8edd0d8836eb050a9a152
amitay biton <amitaybiton@gmail.com>
2021-12-25 12:08:45 +0200

    register - added validation and password complexity

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "Yuval0547.",
+  PASSWORD: "password",
   DB: "vulnwebsitedb",
 };
--- api/routes/register.js
+++ api/routes/register.js
@@ -1,22 +1,49 @@
+const e = require('express');
 var express = require('express');
 var router = express.Router();
+var databaseConnection = require("../handlers/db");
+const passwordComplexity = require("joi-password-complexity");
+const PWD_CONFIG = require("../config/pwd.config");
 
 router.post('/', (req, res) => {
-    // Insert Register Code Here
     // TODO: implement sql query to Database, hashing password and check if password hash equal to database
-    if(req.body.username && req.body.password && req.body.emailAddress){
-        let username = req.body.username;
-        let password = req.body.password;
-        let emailAddress = req.body.emailAddress;
-        
+    
+    if(req.body.username && req.body.password && req.body.emailAddress && req.body.firstName && req.body.lastName){
         //input validation:
-        // TODO: create config file with password strength policy 
-        
-        // insert registration to SQL Table
+        // password validation:
+        var passwordValidation = passwordComplexity(PWD_CONFIG).validate(req.body.password)
+        if(passwordValidation.hasOwnProperty('error')){
+            var validationMessage = passwordValidation.error.details[0].message
+            res.status(400).send(`ValidationErr: ${validationMessage}`)
+        } else{
+            // isExist validation:
+            databaseConnection.query(`SELECT emailAddress FROM users WHERE username = '${req.body.username}'`, function (err, result) {
+                if (err) throw err;
+                if (result.length == 0) {
+                    //all valid !
+                    // hasing password:
+                    var passwordHash = hashPassword(req.body.password)
 
-    }else{
+                    //db insersion:
+                    databaseConnection.query(`INSERT INTO vulnwebsitedb.users(userName,passwordHash,lastName,firstName,emailAddress)
+                    VALUES ('${req.body.username}','${passwordHash}','${req.body.lastName}','${req.body.firstName}','${req.body.emailAddress}')`, function (err, result) {
+                        if (err) throw err;
+                        res.status(200).send(`{"customerID": ${result.insertId}}`);
+                    });
+                }
+                else{
+                    res.status(400).send(`the user '${req.body.username}' is already exists`);
+                }
+            });
+        }        
+    } else{
         res.send("One or more parameters are not provided. Required parameters:'username','password','emailAddress'");
     }
+    
   });
 
 module.exports = router;
+
+function hashPassword(clearPassword){
+    return clearPassword
+}
